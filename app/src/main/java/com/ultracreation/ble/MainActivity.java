97a57d39feafb5bb77f63f60d3f05decdc5bc29e
youqw <m18744017952@163.com>
2016-12-31 22:01:13 +0800

    test

--- app/src/main/java/com/ultracreation/ble/MainActivity.java
+++ app/src/main/java/com/ultracreation/ble/MainActivity.java
@@ -3,6 +3,8 @@ package com.ultracreation.ble;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.ultracreation.blelib.tools.TGattScaner;
@@ -10,16 +12,24 @@ import com.ultracreation.blelib.tools.TShell;
 import com.ultracreation.blelib.tools.TShellRequest;
 import com.ultracreation.blelib.utils.KLog;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity
 {
     private final static String TAG = MainActivity.class.getSimpleName();
     private final int REQUEST_ENABLE_BT = 1;
+
+    @BindView(R.id.test)
+    Button test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         String[] filters = {".blt", "bluetensx", "bluetensq"};
         TGattScaner.Scaner.initBluetooth(this, REQUEST_ENABLE_BT);
@@ -49,6 +59,26 @@ public class MainActivity extends AppCompatActivity
             });
         });
     }
+
+    @OnClick({R.id.test})
+    void onClick(View v)
+    {
+        TShell.Shell.versionRequest(new TShellRequest.RequestListener()
+        {
+            @Override
+            public void onSuccessful(String value)
+            {
+                KLog.i(TAG, "ver:" + value);
+            }
+
+            @Override
+            public void onFailed(String err)
+            {
+                KLog.i(TAG, "error:" + err);
+            }
+        });
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
